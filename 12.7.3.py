fc5b63bea8bdc6d5accd080c4f3227a8e1a46831
aleoshka <macarena7777777@gmail.com>
2023-04-09 18:24:44 +0300

    update

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,10 +1,10 @@
 money = input('Какую сумму Вы хотите положить? ')
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-a = per_cent['ТКБ']/100*int(money)
-b = per_cent['СКБ']/100*int(money)
-c = per_cent['ВТБ']/100*int(money)
-d = per_cent['СКБ']/100*int(money)
-R = a, b, c, d
-deposit = list(map(round, R))
+tkb = per_cent['ТКБ']/100*int(money)
+skb = per_cent['СКБ']/100*int(money)
+vtb = per_cent['ВТБ']/100*int(money)
+sbr = per_cent['СКБ']/100*int(money)
+total = tkb, skb, vtb, sbr
+deposit = list(map(round, total))
 print(deposit)
 print('Максимальная сумма, которую Вы можете заработать —', max(deposit))
